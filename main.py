52251f8c1995da8f219bfc3687f83f19afe19cee
Gama <88944546+Code0008@users.noreply.github.com>
2024-05-25 21:40:26 -0500

    Update main.py
    Limpiando y reescribiendo el main.py

--- main.py
+++ main.py
@@ -1,34 +1,29 @@
-import requests
-import os 
-import webbrowser
+import make_login as mk
 import threading
 import sys
-## funciones abajo
-def createfile(url):
-    x = requests.request('GET',url )
-    texts=x.text
-    with open('login.html', 'w', encoding='utf8') as login:
-        login.write(str(texts))   
-opene= lambda D: webbrowser.open('http://localhost:8080/scraping/login.html')
-x= lambda D: os.system("python3 -m  http.server 8080")
-## funciones arrriba
 
-if __name__ == '__main__':
+
+def main():
     try:
         url=input("[+] Ingrese la URL: ")
         urlv= lambda url: url if 'http:' or 'https:' in url else 'x'
         if urlv(url):print("URL CORRECTA")
         else: print("url incorrecta"), sys.exit(1)
     except ValueError as v:
-        print(f'Ingresa corretamente la url oe')
+            print(f'Ingresa corretamente la url oe')
     except KeyboardInterrupt as k:
-        print(f'Interferida por el usuario')
-tareas=[]
-works=[opene, createfile, x]
-try:
-    for n in works:
-        hilo=threading.Thread(target=n, args=(url,))
-        hilo.start()
-        tareas.append(hilo)
-except KeyboardInterrupt as e:
-    print(f"{e}Proceso terminado por el usuario")
+            print(f'Interferida por el usuario')
+
+    tareas=[]
+    works=[mk.opene, mk.createfile, mk.x]
+    
+    try:
+        for n in works:
+            hilo=threading.Thread(target=n, args=(url,))
+            hilo.start()
+            tareas.append(hilo)
+    except KeyboardInterrupt as e:
+        print(f"{e}Proceso terminado por el usuario")
+
+if __name__ == '__main__':
+    main()
